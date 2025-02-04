65e5d401838cf133f5981a89ccc4b40f65bd7947
huzefa <huzefa.b@incubyte.co>
2025-02-04 19:20:58 +0530

    Add case when input is a single number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,7 +6,13 @@ RSpec.describe StringCalculator do
   describe '#add' do
     context 'when input is an empty string' do
       it 'returns 0' do
-        expect(calculator.add("")).to eq(0)
+        expect(calculator.add('')).to eq(0)
+      end
+    end
+
+    context 'when input is a single number' do
+      it 'returns the number itself' do
+        expect(calculator.add('1')).to eq(1)
       end
     end
   end
