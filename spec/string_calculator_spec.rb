986f2ea5662d946c42babbcc08ef986cb158939c
huzefa <huzefa.b@incubyte.co>
2025-02-04 19:30:10 +0530

    Add case for input containing multiple numbers - this will pass already as it is handled in previous case logic

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -21,5 +21,11 @@ RSpec.describe StringCalculator do
         expect(calculator.add('1,5')).to eq(6)
       end
     end
+
+    context 'when input contains multiple numbers' do
+      it 'returns their sum' do
+        expect(calculator.add('1,2,3,4')).to eq(10)
+      end
+    end
   end
 end
